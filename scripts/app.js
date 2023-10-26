bce7287cf76e78dea86283acd709b2d2952b0d1d
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-26 19:27:44 +0100

    Add javascript

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,21 @@ const counterText = document.getElementById('counterText');
 const decreaseButton = document.getElementsByClassName('js-decrease-button')[0];
 const resetButton = document.querySelector('.js-reset-button');
 const increaseButton = document.querySelectorAll('.js-increase-button')[0];
+
+function increaseValue() {
+  counterText.innerText = String(Number(counterText.innerText) + 1);
+}
+
+function decreaseValue() {
+  counterText.innerText = String(Number(counterText.innerText) - 1);
+}
+
+function resetValue() {
+  counterText.innerText = '0';
+}
+
+// event listeners
+
+increaseButton.addEventListener('click', increaseValue);
+decreaseButton.addEventListener('click', decreaseValue);
+resetButton.addEventListener('click', resetValue);
