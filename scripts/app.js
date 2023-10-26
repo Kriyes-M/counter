b8c5ecf63ad936cc237d437adfcad997e74b3e60
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-26 19:15:05 +0100

    Access elements in js

---
+++ scripts/app.js
@@ -0,0 +1,4 @@
+const counterText = document.getElementById('counterText');
+const decreaseButton = document.getElementsByClassName('js-decrease-button')[0];
+const resetButton = document.querySelector('.js-reset-button');
+const increaseButton = document.querySelectorAll('.js-increase-button')[0];
